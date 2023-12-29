53a87ce003145577db6ef773cb1738a6531a0966
Chathura <cpriydasun@gmail.com>
2023-12-30 02:32:19 +0530

    all Front End structures done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateEvent from "./Components/CreateEvent/CreateEvent";
+import Uploads from "./Components/Uploads/Uploads";
+import Requests from "./Components/Requests/Requests";
+import Comments from "./Components/Comments/Comments";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/createEvent" element={<CreateEvent />} />
+      </Routes>
+      <Routes>
+        <Route path="/uploads" element={<Uploads />} />
+      </Routes>
+      <Routes>
+        <Route path="/requests" element={<Requests />} />
+      </Routes>
+      <Routes>
+        <Route path="/comments" element={<Comments />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
