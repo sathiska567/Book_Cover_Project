579059a01398421d88ea57b2bba8023518142729
Chathura <cpriydasun@gmail.com>
2024-01-09 03:46:36 +0530

    Footer completed Mobile and desktop

--- client/src/Components/UserWebSite/UserWebSite.jsx
+++ client/src/Components/UserWebSite/UserWebSite.jsx
@@ -13,6 +13,10 @@ import Gallery from "./Gallery";
 import Request from "./Request";
 import YoutubeWidget from "./YoutubeWidget";
 import Author from "./Author";
+import { Button } from "antd";
+import { SocialIcon } from "react-social-icons";
+import "react-social-icons/vimeo";
+import "react-social-icons/meetup";
 
 const UserWebSite = () => {
 
@@ -60,46 +64,61 @@ const UserWebSite = () => {
                 <Nav.Link className={userStyles.NavLink} href="#Author">
                   About Author
                 </Nav.Link>
-
                 {isMobile ? (
                   <>
                     <hr />
                     <Nav.Link className={userStyles.NavLink} href="#action/3.2">
                       My Profile
                     </Nav.Link>
-                    <Nav.Link className={userStyles.NavLink} href="#action/3.3">
+                    <Nav.Link className={userStyles.NavLink} href="/login">
+                      Register
+                    </Nav.Link>
+                    <Nav.Link className={userStyles.NavLink} href="/login">
                       Login
                     </Nav.Link>
-                    <Nav.Link className={userStyles.NavLink} href="#action/3.3">
+                    <Nav.Link className={userStyles.NavLink} href="#">
                       Log Out
                     </Nav.Link>
                   </>
                 ) : (
-                  <NavDropdown
-                    className={userStyles.NavLink}
-                    title="My Profile"
-                    id="basic-nav-dropdown"
-                  >
-                    <NavDropdown.Item
-                      className={userStyles.dropItems}
-                      href="#action/3.2"
+                  <>
+                    <NavDropdown
+                      className={userStyles.NavLink}
+                      title="My Profile"
+                      id="basic-nav-dropdown"
                     >
-                      <UserOutlined className={userStyles.navBarIcon} /> My
-                      Profile
-                    </NavDropdown.Item>
-                    <Dropdown.Divider />
-                    <NavDropdown.Item
-                      className={userStyles.dropItems}
-                      href="#action/3.3"
-                    >
-                      <LoginOutlined className={userStyles.navBarIcon} /> Login
-                    </NavDropdown.Item>
+                      <NavDropdown.Item
+                        className={userStyles.dropItems}
+                        href="#action/3.2"
+                      >
+                        <UserOutlined className={userStyles.navBarIcon} /> My
+                        Profile
+                      </NavDropdown.Item>
 
-                    <NavDropdown.Item className={userStyles.dropItems}>
-                      <LogoutOutlined className={userStyles.navBarIcon} />{" "}
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
+                      <Dropdown.Divider />
+                      <NavDropdown.Item
+                        className={userStyles.dropItems}
+                        href="/login"
+                      >
+                        <LoginOutlined className={userStyles.navBarIcon} />{" "}
+                        Login
+                      </NavDropdown.Item>
+
+                      <NavDropdown.Item
+                        className={userStyles.dropItems}
+                        href="#"
+                      >
+                        <LogoutOutlined className={userStyles.navBarIcon} />{" "}
+                        Logout
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link
+                      href="/signup"
+                      className={`${userStyles.NavLink} ${userStyles.RegisterButton}`}
+                    >
+                      Register
+                    </Nav.Link>
+                  </>
                 )}
               </Nav>
             </Navbar.Collapse>
@@ -130,6 +149,11 @@ const UserWebSite = () => {
             </div>
           </div>
         </div>
+        <svg className={userStyles.arrows}>
+          <path className={userStyles.a1} d="M0 0 L30 32 L60 0"></path>
+          <path className={userStyles.a2} d="M0 20 L30 52 L60 20"></path>
+          <path className={userStyles.a3} d="M0 40 L30 72 L60 40"></path>
+        </svg>
       </div>
 
       <div id="Gallery" className={userStyles.galleryContainer}>
@@ -159,6 +183,87 @@ const UserWebSite = () => {
           <Author />
         </div>
       </div>
+      <footer>
+        <div className={userStyles.FooterContainer}>
+          <div className={userStyles.FooterLogo}>
+            <img
+              style={{
+                width: "100px",
+                height: "100px",
+              }}
+              src="./e.png"
+              alt="logo"
+            />
+          </div>
+          <div className={userStyles.FooterNavigation}>
+            <a href="#">Home</a>
+            <a href="#Gallery">Gallery</a>
+            <a href="#Request">Request Reading</a>
+            <a href="#YouTubeWidget">YouTube Channel</a>
+            <a href="">Reviews</a>
+            <a href="#Author">About Author</a>
+            <Button
+              type="primary"
+              href="/signup"
+              className={userStyles.FooterButton}
+            >
+              Register
+            </Button>
+          </div>
+          <div className={userStyles.FooterSocialLinks}>
+            <SocialIcon url="www.facebook.com" />
+            <SocialIcon url="www.youtube.com" />
+            <SocialIcon url="www.linkedin.com" />
+            <SocialIcon url="www.gmail.com" />
+          </div>
+        </div>
+
+        <div className={userStyles.mobileFooterContainer}>
+          <div className={userStyles.mobileNavigation}>
+            <a href="#">Home</a>
+            <a href="#Gallery">Gallery</a>
+            <a href="#Request">Request Reading</a>
+            <a href="#YouTubeWidget">YouTube Channel</a>
+            <a href="">Reviews</a>
+            <a href="#Author">About Author</a>
+            <Button
+              type="primary"
+              href="/signup"
+              className={userStyles.FooterMobileButton}
+            >
+              Register
+            </Button>
+          </div>
+          <div className={userStyles.FooterSocialLinksMobile}>
+            <SocialIcon
+              bgColor="transparent"
+              userStyles
+              className={userStyles.SocialIcon}
+              url="https://www.facebook.com"
+            />
+            <SocialIcon
+              bgColor="transparent"
+              userStyles
+              className={userStyles.SocialIcon}
+              url="www.email.com"
+            />
+            <SocialIcon
+              bgColor="transparent"
+              userStyles
+              className={userStyles.SocialIcon}
+              url="https://instagram.com"
+            />
+            <SocialIcon
+              bgColor="transparent"
+              className={userStyles.SocialIcon}
+              url="https://linkedin.com"
+            />
+          </div>
+        </div>
+        <p className={userStyles.copyright}>
+          ©2024 Design by CodeVanguard | All Rights Reserved
+        </p>
+      </footer>
     </div>
   );
 };
